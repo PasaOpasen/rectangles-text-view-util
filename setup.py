576479aa54bec0d9e5cba6bfa2955fe61936e7d4
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-11-28 10:24:39 +0300

    update to 0.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ordered_rectangles", 
-    version="0.0.2",
+    version="0.0.3",
     author="Demetry Pascal",
     author_email="qtckpuhdsa@gmail.com",
     maintainer='Demetry Pascal',
